f91be1be69f36df98f60de0ad7835defe69db0dc
Yann Carvalho <yanncarvalho95@gmail.com>
2023-09-15 20:45:14 -0300

    py

--- lista_exercicios_ii/questao3.py
+++ lista_exercicios_ii/questao3.py
@@ -1,4 +1,5 @@
-# 3. Crie uma classe chamada “Retangulo” que tenha atributos “base” e “altura”. Implemente um método chamado “calcular_area” que retorna a área do retângulo.
+# 3. Crie uma classe chamada “Retangulo” que tenha atributos “base” e “altura”. Implemente um método chamado
+# “calcular_area” que retorna a área do retângulo.
 
 class Retangulo:
     def __init__(self, base, altura):
